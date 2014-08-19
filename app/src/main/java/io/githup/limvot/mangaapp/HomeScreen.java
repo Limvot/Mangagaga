132d272f6188dca3c04abbf742b369135a537139
Nathan Braswell <miloignis@gmail.com>
2014-08-19 18:12:23 -0400

    Initial commit.

---
+++ app/src/main/java/io/githup/limvot/mangaapp/HomeScreen.java
@@ -0,0 +1,55 @@
+package io.githup.limvot.mangaapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.luaj.vm2.*;
+import org.luaj.vm2.lib.jse.*;
+
+import java.io.StringReader;
+
+
+public class HomeScreen extends Activity {
+
+    TextView scriptView;
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        scriptView = (TextView) findViewById(R.id.script_box);
+        scriptView.append("\n");
+
+        String luaFunctionString = "function Echo(it) return 'Lua: ' .. it end";
+
+        Globals globals = JsePlatform.standardGlobals();
+        globals.load(new StringReader(luaFunctionString), "main.lua").call();
+        LuaValue echoFun = globals.get("Echo");
+        scriptView.append(echoFun.call(LuaValue.valueOf("Java String!")).toString());
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
