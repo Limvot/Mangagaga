bd0b15e90d8f19bc96fd00e907cd342e497eca36
Marcus Godwin <marcusgodwin@hotmail.com>
2014-11-04 16:19:47 -0500

    Images should be resized to a size that the device can handle before rendering them via opengl now.

--- app/src/main/java/io/githup/limvot/mangaapp/ImageManager.java
+++ app/src/main/java/io/githup/limvot/mangaapp/ImageManager.java
@@ -3,6 +3,11 @@ package io.githup.limvot.mangaapp;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+import javax.microedition.khronos.opengles.GL10;
+
+import android.graphics.Matrix;
+import android.opengl.GLES10;
+
 /**
  * Created by marcus on 8/22/14.
  */
@@ -36,6 +41,18 @@ public class ImageManager {
 
     public Bitmap load(String path)
     {
-        return bitmapMaker.decodeFile(path);
+        int[] textureSize = new int[1];
+        GLES10.glGetIntegerv(GLES10.GL_MAX_TEXTURE_SIZE, textureSize, 0);
+
+        Bitmap bm = bitmapMaker.decodeFile(path);
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaledW = ((float) textureSize[0])/width;
+        float scaledH = ((float) textureSize[0])/height;
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaledW, scaledH);
+        Bitmap resizedBm = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
+        bm.recycle();
+        return resizedBm;
     }
 }
