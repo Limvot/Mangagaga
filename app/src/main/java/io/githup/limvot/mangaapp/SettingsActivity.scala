0dbbd5089a14f0008696239266fefffc73c8ba09
Marcus Godwin <marcusgodwin@hotmail.com>
2014-11-15 18:46:04 -0500

    Ported Settings Activity to scala.

---
+++ app/src/main/java/io/githup/limvot/mangaapp/SettingsActivity.scala
@@ -0,0 +1,67 @@
+package io.githup.limvot.mangaapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.scaloid.common._
+
+class SettingsActivity extends SActivity {
+
+    implicit val tag = LoggerTag("Scala Setting Activity")
+    private val manager : SettingsManager = SettingsManager.getSettingsManager();
+    var historySize : STextView = null
+    var editText : SEditText = null
+
+    onCreate{
+        contentView = new SVerticalLayout() {
+            val buttonClearHistory = SButton("Clear History",MangaManager.getMangaManager().clearHistory())
+            val buttonClearCache = SButton("Clear Cache",Utilities.clearCache())
+            val buttonClearFavorites = SButton("Clear Favorites",MangaManager.getMangaManager().clearFavorites())
+            val buttonClearSaved = SButton("Clear Saved Chapters",MangaManager.getMangaManager().clearSaved())
+            val buttonClearAll = SButton("Clear All",clearAll())
+            historySize = STextView("Number of entries to save in history")
+            editText = SEditText()
+            editText.afterTextChanged(historyLimitCallback())
+        }
+    }
+
+    def clearAll() {
+        Utilities.clearCache()
+        MangaManager.getMangaManager().clearHistory()
+        MangaManager.getMangaManager().clearFavorites()
+        MangaManager.getMangaManager().clearSaved()
+    }
+
+
+
+    /*override def onCreateOptionsMenu(menu : Menu) : Boolean = {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.settings, menu);
+        return true;
+    }
+
+
+    override def onOptionsItemSelected(item : MenuItem) : Boolean = {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
+
+    def historyLimitCallback() {
+        SettingsManager.setHistorySize(Integer.parseInt(editText.getText().toString()))
+        info(Integer.toString(SettingsManager.getHistorySize()))
+    }
+}
