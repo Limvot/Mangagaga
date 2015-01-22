ebad9e8fed07cfd423efb5fd367c3763687297aa
Nathan Braswell <nathan@nathanbraswell.com>
2015-01-22 12:22:06 -0500

    rip out a lot of cruft - including the last bit of Java! Flinging seems to work better now, as there are less event listeners that could intercept them.

--- app/src/main/java/io/githup/limvot/mangaapp/ImageViewerActivity.scala
+++ app/src/main/java/io/githup/limvot/mangaapp/ImageViewerActivity.scala
@@ -1,7 +1,5 @@
 package io.githup.limvot.mangaapp;
 
-import io.githup.limvot.mangaapp.util.SystemUiHider;
-
 import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.app.Activity;
@@ -58,16 +56,6 @@ class ImageViewerActivity extends Activity with GestureDetector.OnGestureListene
      */
     private val TOGGLE_ON_CLICK: Boolean = true;
 
-    /**
-     * The flags to pass to {@link SystemUiHider#getInstance}.
-     */
-    private val HIDER_FLAGS: Int = SystemUiHider.FLAG_HIDE_NAVIGATION;
-
-    /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
-    private var mSystemUiHider:SystemUiHider = null;
-
     private var detector:GestureDetectorCompat = null;
     private var gestureListen:View.OnTouchListener = null;
 
@@ -90,63 +78,10 @@ class ImageViewerActivity extends Activity with GestureDetector.OnGestureListene
           actionBar.hide();
         }
 
-
-
-        val controlsView: View = findViewById(R.id.fullscreen_content_controls);
         val contentView: View = findViewById(R.id.fullscreen_content);
 
         detector = new GestureDetectorCompat(this,this);
 
-        // Set up an instance of SystemUiHider to control the system UI for
-        // this activity.
-        mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
-        mSystemUiHider.setup();
-        mSystemUiHider.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-                    // Cached values.
-                    var mControlsHeight: Int = 0;
-                    var mShortAnimTime: Int = 0;
-
-                    
-                    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-                    override def onVisibilityChange(visible: Boolean) =  {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-                            // If the ViewPropertyAnimator API is available
-                            // (Honeycomb MR2 and later), use it to animate the
-                            // in-layout UI controls at the bottom of the
-                            // screen.
-                            if (mControlsHeight == 0) {
-                                mControlsHeight = controlsView.getHeight();
-                            }
-                            if (mShortAnimTime == 0) {
-                                mShortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-                            }
-                            controlsView.animate().translationY((if (visible) 0 else mControlsHeight)).setDuration(mShortAnimTime);
-                        } else {
-                            // If the ViewPropertyAnimator APIs aren't
-                            // available, simply show or hide the in-layout UI
-                            // controls.
-                            controlsView.setVisibility((if (visible) View.VISIBLE else 8));
-                            // replaced GONE with value 8. Looked up value in dev documentation. 
-                            // Value is listed as 0x0000000008
-                        }
-
-                        if (visible && AUTO_HIDE) {
-                            // Schedule a hide().
-                            delayedHide(AUTO_HIDE_DELAY_MILLIS);
-                        }
-                    }
-                });
-
-        // Set up the user interaction to manually show or hide the system UI.
-        contentView.setOnClickListener(new View.OnClickListener() {
-            override def onClick(view: View) {
-                if (TOGGLE_ON_CLICK) {
-                    mSystemUiHider.toggle();
-                } else {
-                    mSystemUiHider.show();
-                }
-            }
-        });
 
         contentView.setOnTouchListener(new View.OnTouchListener() {
             override def onTouch(view: View, motionEvent: MotionEvent): Boolean =  {
@@ -162,14 +97,14 @@ class ImageViewerActivity extends Activity with GestureDetector.OnGestureListene
         //findViewById(R.id.back_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
-    override protected def onPostCreate(savedInstanceState: Bundle) = {
-        super.onPostCreate(savedInstanceState);
+    //override protected def onPostCreate(savedInstanceState: Bundle) = {
+        //super.onPostCreate(savedInstanceState);
 
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        //delayedHide(100);
-    }
+        //// Trigger the initial hide() shortly after the activity has been
+        //// created, to briefly hint to the user that UI controls
+        //// are available.
+        ////delayedHide(100);
+    //}
 
     /**
      * Gesture Listener method implementations.
@@ -246,60 +181,23 @@ class ImageViewerActivity extends Activity with GestureDetector.OnGestureListene
             }
         }
         updateImage();
-        return true;
+        true
     }
 
-    def onDown(event: MotionEvent): Boolean = {
-        return true;
-    }
-
-
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    var mDelayHideTouchListener = new View.OnTouchListener() {
-        override def onTouch(view: View, motionEvent: MotionEvent): Boolean = {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
-
-    var mHideHandler = new Handler();
-    var mHideRunnable = new Runnable() {
-        override def run() = {
-            mSystemUiHider.hide();
-        }
-    };
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private def delayedHide(delayMillis: Int) = {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
-    }
+    def onDown(event: MotionEvent) = true
 
     // Do image updates async to maintain responsiveness
-
-
     def updateImage() = {
-        // new UpdateImage().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
         // THE FUTURE IS NOW
         Future {
-            Log.i("ASYNC DO IN BACKGROUND", "STARTING");
-            var imagepath = MangaManager.getCurrentPage();
-            Log.i("Display image!", imagepath);
-            Log.i("ASYNC DO IN BACKGROUND", "ENDING");
-            var bm: Bitmap = ImageManager.getNext(imagepath);
-            Log.i("ASYNC DO IN BACKGROUND", "Post Execute!");
-            runOnUiThread(contentview.setImageBitmap(bm));
+            Log.i("ASYNC DO IN BACKGROUND", "STARTING")
+            var imagepath = MangaManager.getCurrentPage()
+            Log.i("Display image!", imagepath)
+            Log.i("ASYNC DO IN BACKGROUND", "ENDING")
+            var bm: Bitmap = ImageManager.getNext(imagepath)
+            Log.i("ASYNC DO IN BACKGROUND", "Post Execute!")
+            runOnUiThread(contentview.setImageBitmap(bm))
         }
         // NOW IT'S THE PRESENT
-
     }
 }
