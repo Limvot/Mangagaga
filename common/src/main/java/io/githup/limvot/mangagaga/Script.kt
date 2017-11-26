94dd7aa7eac2ac41e29e6c4d52e5affe0b5f7d1b
Nathan Braswell <miloignis@gmail.com>
2017-11-25 21:36:10 -0500

    use discriptive file names

--- common/src/main/java/io/githup/limvot/mangagaga/Script.kt
+++ common/src/main/java/io/githup/limvot/mangagaga/Script.kt
@@ -10,8 +10,8 @@ class Script(val name : String, val code : String, val scriptNumber : Int) : Gen
         try {
             scriptScope = cx.initStandardObjects()
             ScriptableObject.putProperty(scriptScope, "api", APIObject.instance())
-            cx.evaluateString(scriptScope, ScriptManager.codePrequel, "<cmd>", 1, null)
-            cx.evaluateString(scriptScope, code, "<cmd>", 1, null)
+            cx.evaluateString(scriptScope, ScriptManager.codePrequel, "codePrequel", 1, null)
+            cx.evaluateString(scriptScope, code, name, 1, null)
         } finally {
             Context.exit()
         }
